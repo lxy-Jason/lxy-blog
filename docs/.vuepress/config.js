cbd411ed781cae180b4902428f138b89981d2e82
lxy-Jason <1627962483@qq.com>
2023-04-15 20:04:38 +0800

    feat: 项目初始化&导航栏配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: 李湘粤
+ * @Date: 2023-04-15 18:34:15
+ * @LastEditors: 李湘粤
+ * @LastEditTime: 2023-04-15 19:58:53
+ * @Description: 请填写简介
+ */
+module.exports = {
+  title: "lxy-blog",
+  description: "just a blog",
+  base: "/",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "博客", link: "/blog/" },
+    ],
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: 李湘粤
+ * @Date: 2023-04-13 21:44:56
+ * @LastEditors: 李湘粤
+ * @LastEditTime: 2023-04-15 19:45:04
+ * @Description: 请填写简介
+-->
+# Hello VuePress
