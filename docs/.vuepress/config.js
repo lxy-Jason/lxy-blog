15b82d4a45b5ef0b504c71d93fa8a4d9115138f1
lxy-Jason <1627962483@qq.com>
2023-04-17 22:50:42 +0800

    feat: 侧边栏设置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: 李湘粤
  * @Date: 2023-04-15 18:34:15
  * @LastEditors: 李湘粤
- * @LastEditTime: 2023-04-15 19:58:53
+ * @LastEditTime: 2023-04-17 22:46:56
  * @Description: 请填写简介
  */
 module.exports = {
@@ -14,5 +14,19 @@ module.exports = {
       { text: "首页", link: "/" },
       { text: "博客", link: "/blog/" },
     ],
+    sidebar:{
+      '/blog/':[
+        {
+          'title':'前端',
+          'collapsable':false, //是否可折叠
+          children:[
+            ['css/test','垂直居中'],
+            ['css/margin穿透','margin穿透'],
+            ['css/盒模型','盒模型'],
+            ['css/两栏布局','两栏布局'],
+          ]
+        }
+      ]
+    }
   },
 };
---
+++ docs/blog/css/test.md
@@ -0,0 +1,33 @@
+<!--
+ * @Author: 李湘粤
+ * @Date: 2023-04-17 22:45:48
+ * @LastEditors: 李湘粤
+ * @LastEditTime: 2023-04-17 22:45:52
+ * @Description: 请填写简介
+-->
+### 定位+margin
+
+position:absolute
+
+然后设置top,right,bottom,right全为0
+
+margin设置成auto
+
+### 定位+transform
+
+position:absolute
+
+top:50%
+
+left:50%
+
+transform:translate(-50%,-50%)
+
+### flex
+
+父盒子display:flex
+
+justify-content:center
+
+item-align:center
+
